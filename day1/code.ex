ac6a8f9b57733473402dd00cf30a565332bc6741
Bartosz Pietrzak <bartosz.pietrzak@gamivo.com>
2021-12-02 14:26:32 +0100

    wip

--- day1/code.ex
+++ day1/code.ex
@@ -1,17 +1,42 @@
 defmodule AdventOfCode do
 
-  def run() do
+  defp input do
     {:ok, data} = File.read("input.txt")
     data
     |> String.split("\n")
     |> Enum.map(fn i -> String.to_integer(i) end)
+  end
+
+  defp count_incrementing(list) when is_list(list) do
+    list
     |> Enum.reduce({0, nil}, fn i, acc ->
       if elem(acc, 1) != nil and i > elem(acc, 1), do: {elem(acc, 0) + 1, i}, else: {elem(acc, 0), i}
     end)
     |> elem(0)
-    |> IO.puts
+  end
+
+  def run_part1() do
+    input()
+    |> count_incrementing()
+  end
+
+  def run_part2() do
+    input = input()
+    input
+    |> Enum.reduce({0, []}, fn _i, acc ->
+      offset = elem(acc, 0)
+      case Enum.slice(input, offset, 3) do
+        list when is_list(list) and length(list) == 3 -> {offset+1, [list | elem(acc, 1)]}
+        _ -> {offset+1, elem(acc, 1)}
+      end
+    end)
+    |> elem(1)
+    |> Enum.reverse()
+    |> Enum.map(fn item -> Enum.sum(item) end)
+    |> count_incrementing()
   end
 
 end
 
-AdventOfCode.run()
+IO.puts("Part1: #{AdventOfCode.run_part1()}")
+IO.puts("Part2: #{AdventOfCode.run_part2()}")
